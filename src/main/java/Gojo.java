a232f480177220e73cd281828a6810bb83d73784
WayneCh0y <>
2025-01-28 01:13:20 +0800

    Implement Add, List

--- src/main/java/Gojo.java
+++ src/main/java/Gojo.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Gojo {
+    public static void printList(List<String> listItems) {
+        int index = 0;
+        System.out.println("____________________________________________________________");
+        for (String items : listItems) {
+            System.out.println((index + 1) + ". " + items);
+            index++;
+        }
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         String line;
+        List<String> listItems = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
         System.out.println("Hey there! The name's Gojo. The strongest sorcerer you'll ever meet.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
         while(true) {
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
             }
+            if (line.equals("list")) {
+                printList(listItems);
+                continue;
+            }
             System.out.println("____________________________________________________________");
-            System.out.println(line);
+            System.out.println("added: " + line);
+            listItems.add(line);
             System.out.println("____________________________________________________________");
         }
         System.out.println("Bye! Don't miss me too much, okay?");
