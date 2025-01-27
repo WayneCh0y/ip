dc4c25a3ffffaa8198dc71ba2287fbcd4c40c7c4
WayneCh0y <>
2025-01-28 00:46:57 +0800

    Implementing Echo

--- src/main/java/Gojo.java
+++ src/main/java/Gojo.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Gojo {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Gojo");
+        System.out.println("Hey there! The name's Gojo. The strongest sorcerer you'll ever meet.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        while(true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
+        System.out.println("Bye! Don't miss me too much, okay?");
         System.out.println("____________________________________________________________");
     }
 }
